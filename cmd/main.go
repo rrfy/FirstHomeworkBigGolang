16ae6d59b51953411d2e28f5aeeef2a0e525a456
Stepan <sshkidin@yandex.ru>
2024-10-20 14:58:30 +0300

    added server

--- cmd/main.go
+++ cmd/main.go
@@ -1,33 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"homework1/internal/pkg/server"
 	"homework1/internal/pkg/storage"
-	"os"
 )
 
 func main() {
-	ls := storage.NewListStorage()
-
-	// Загрузка состояния с диска
-	if err := ls.LoadFromDisk("db.json"); err != nil && !os.IsNotExist(err) {
-		fmt.Println("Error loading state:", err)
-	}
-
-	// Примеры работы с базой данных
-	ls.RPUSH("mylist", []string{"a", "b", "c"})
-	ls.LSET("mylist", 1, "z")
-
-	value, err := ls.LGET("mylist", 1)
+	store, err := storage.NewStorage()
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("Value at index 1:", value)
+		panic(err)
 	}
 
-	// Сохранение состояния на диск перед выходом
-	if err := ls.SaveToDisk("db.json"); err != nil {
-		fmt.Println("Error saving state:", err)
-	}
-
+	s := server.New(":8090", &store)
+	s.Start()
 }
